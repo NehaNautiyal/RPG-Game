4fb73ec471a9618c3c1b528bbc8d36a7c8c24dab
Neha Nautiyal <nehahaha@gmail.com>
2019-02-22 16:13:52 -0500

    more work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,115 +10,143 @@ $(document).ready(function () {
     var m4hp = 1913;
 
     //When I click on Model 1 (Models 2, 3, 4, text, & buttons) move
-    $(".model-1").on("click", function () {
-        console.log("I am model 1.");
-        //model-1 is going to move down
-        $(".model-2").animate({
-            top: '270px',
-            right: '140px'
+        $(".model-1").on("click", function () {
+            console.log("I am model 1.");
+            //model-1 is going to move down
+            $(".model-2").animate({
+                top: '270px',
+                right: '140px'
+            });
+            $(".model-2").css('background', '#64CFD2');
+            $(".model-3").animate({
+                top: '270px',
+                right: '140px'
+            });
+            $(".model-3").css('background', '#64CFD2');
+            $(".model-4").animate({
+                top: '270px',
+                right: '140px'
+            });
+            $(".model-4").css('background', '#64CFD2');
+            $("#disprove").animate({
+                top: '240px'
+            });
+            $("#expSection").animate({
+                top: '240px'
+            });
+            $(".btn").animate({
+                top: '240px'
+            });
+            $("#defender").animate({
+                top: '240px'
+            });
+            console.log("I can make anything move.");
         });
-        $(".model-3").animate({
-            top: '270px',
-            right: '140px'
-        });
-        $(".model-4").animate({
-            top: '270px',
-            right: '140px'
-        });
-        $("#disprove").animate({
-            top: '240px'
-        });
-        $("#expSection").animate({
-            top: '240px'
-        });
-        $(".btn").animate({
-            top: '240px'
-        });
-        $("#defender").animate({
-            top: '240px'
-        });
-        console.log("I can make anything move.");
-    });
+    
 
     //When Model 2 is clicked, Models 1,3,4,text & btn move
-    $(".model-2").on("click", function () {
-        console.log("I am model 2.");
-        $(".model-1").animate({
-            top: '270px',
+
+        $(".model-2").on("click", function () {
+            console.log("I am model 2.");
+            $(".model-2").animate({
+                right: '140px',
+            });
+            $(".model-1").animate({
+                top: '270px',
+            });
+            $(".model-1").css('background', '#64CFD2');
+            $(".model-3").animate({
+                top: '270px',
+                right: '148px'
+            });
+            $(".model-3").css('background', '#64CFD2');
+            $(".model-4").animate({
+                top: '270px',
+                right: '156px'
+            });
+            $(".model-4").css('background', '#64CFD2');
+            $("#disprove").animate({
+                top: '240px'
+            });
+            $("#expSection").animate({
+                top: '240px'
+            });
+            $(".btn").animate({
+                top: '240px'
+            });
+            $("#defender").animate({
+                top: '240px'
+            });
         });
-        $(".model-3").animate({
-            top: '270px',
-            right: '148px'
-        });
-        $(".model-4").animate({
-            top: '270px',
-            right: '156px'
-        });
-        $("#disprove").animate({
-            top: '240px'
-        });
-        $("#expSection").animate({
-            top: '240px'
-        });
-        $(".btn").animate({
-            top: '240px'
-        });
-        $("#defender").animate({
-            top: '240px'
-        });
-    });
+    
 
     //When Model 3 is clicked, Models 1,2,4,text & btn move
-    $(".model-3").on("click", function () {
-        console.log("I am model 3.");
-        $(".model-1").animate({
-            top: '270px',
+    
+        $(".model-3").on("click", function () {
+            console.log("I am model 3.");
+            $(".model-3").animate({
+                right: '278px',
+            });
+            $(".model-1").animate({
+                top: '270px',
+            });
+            $(".model-1").css('background', '#64CFD2');
+            $(".model-2").animate({
+                top: '270px',
+            });
+            $(".model-2").css('background', '#64CFD2');
+            $(".model-4").animate({
+                top: '270px',
+                right: '148px'
+            });
+            $(".model-4").css('background', '#64CFD2');
+            $("#disprove").animate({
+                top: '240px'
+            });
+            $(".model-4").css('background', '#64CFD2');
+            $("#expSection").animate({
+                top: '240px'
+            });
+            $(".btn").animate({
+                top: '240px'
+            });
+            $("#defender").animate({
+                top: '240px'
+            });
         });
-        $(".model-2").animate({
-            top: '270px',
-        });
-        $(".model-4").animate({
-            top: '270px',
-            right: '148px'
-        });
-        $("#disprove").animate({
-            top: '240px'
-        });
-        $("#expSection").animate({
-            top: '240px'
-        });
-        $(".btn").animate({
-            top: '240px'
-        });
-        $("#defender").animate({
-            top: '240px'
-        });
-    });
-
+    
     //When Model 4 is clicked, Models 1,2,3,text & btn move
-    $(".model-4").on("click", function () {
-        console.log("I am model 4.");
-        $(".model-1").animate({
-            top: '270px',
+  
+        $(".model-4").on("click", function () {
+            console.log("I am model 4.");
+            $(".model-4").animate({
+                right: '432px',
+            });
+            $(".model-1").animate({
+                top: '270px',
+            });
+            $(".model-1").css('background', '#64CFD2');
+            $(".model-2").animate({
+                top: '270px',
+            });
+            $(".model-2").css('background', '#64CFD2');
+            $(".model-3").animate({
+                top: '270px',
+            });
+            $(".model-3").css('background', '#64CFD2');
+            $("#disprove").animate({
+                top: '240px'
+            });
+            $("#expSection").animate({
+                top: '240px'
+            });
+            $(".btn").animate({
+                top: '240px'
+            });
+            $("#defender").animate({
+                top: '240px'
+            });
         });
-        $(".model-2").animate({
-            top: '270px',
-        });
-        $(".model-3").animate({
-            top: '270px',
-        });
-        $("#disprove").animate({
-            top: '240px'
-        });
-        $("#expSection").animate({
-            top: '240px'
-        });
-        $(".btn").animate({
-            top: '240px'
-        });
-        $("#defender").animate({
-            top: '240px'
-        });
-    });
+    
     // If Begin Data collection is clicked, there is no model to disprove should be displayed
 });
